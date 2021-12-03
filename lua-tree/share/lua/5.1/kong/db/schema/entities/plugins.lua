ad445aff2f7e216a2bb24cf94af211ca4364297f
Harry Bagdi <harrybagdi@gmail.com>
2021-12-03 15:01:34 -0800

    feat: introduce an updated_at field on plugin entity

--- lua-tree/share/lua/5.1/kong/db/schema/entities/plugins.lua
+++ lua-tree/share/lua/5.1/kong/db/schema/entities/plugins.lua
@@ -16,6 +16,7 @@ return {
     { id = typedefs.uuid, },
     { name = { type = "string", required = true, }, },
     { created_at = typedefs.auto_timestamp_s },
+    { updated_at = typedefs.auto_timestamp_s },
     { route = { type = "foreign", reference = "routes", default = null, on_delete = "cascade", }, },
     { service = { type = "foreign", reference = "services", default = null, on_delete = "cascade", }, },
     { consumer = { type = "foreign", reference = "consumers", default = null, on_delete = "cascade", }, },
