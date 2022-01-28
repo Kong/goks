71a8d33f48deb519df41e4e04dbddc41496a83f7
Fero <6863207+mikefero@users.noreply.github.com>
2022-01-28 17:14:07 -0500

    feat: return plugin name in call to LoadSchema (#2)

--- plugin/validator.go
+++ plugin/validator.go
@@ -16,9 +16,9 @@ func NewValidator() (*Validator, error) {
 	return &Validator{vm: vm}, nil
 }
 
-func (v *Validator) LoadSchema(schema string) error {
-	_, err := v.vm.CallByParams("load_plugin_schema", schema)
-	return err
+func (v *Validator) LoadSchema(schema string) (string, error) {
+	pluginName, err := v.vm.CallByParams("load_plugin_schema", schema)
+	return pluginName, err
 }
 
 func (v *Validator) Validate(pluginInstance string) error {
