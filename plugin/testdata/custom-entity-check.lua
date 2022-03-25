50ecacff977d0b701d528213aa685f7921c060f3
mikefero <michael.fero@konghq.com>
2022-03-25 15:27:10 -0400

    test: adjust tests for auto field processing in `validate`

--- plugin/testdata/custom-entity-check.lua
+++ plugin/testdata/custom-entity-check.lua
@@ -6,15 +6,14 @@ return {
     {config = {
         type = "record",
         fields = {
-          {strategy = {type = "string", default = "localhost", one_of = { "localhost", "other"}}},
-          {other = {type = "record", fields = {{host = typedefs.host}}}}
+          {strategy = {type = "string", default = "localhost", one_of = { "localhost", "other"}}}
   }}}},
   entity_checks = {{
     custom_entity_check = {
       field_sources = { "config" },
       fn = function(entity)
         local config = entity.config
-        if config.strategy == "other" and (config.other == nil or config.other.host == nil) then
+        if config.strategy == "other" then
           return nil, "custom-entity-check failed message"
         end
         return true
