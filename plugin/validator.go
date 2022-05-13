018bf918f83e1b76efc2f0ec1de3c8c4300e1a17
mikefero <michael.fero@konghq.com>
2022-05-13 10:36:47 -0400

    feat: adding validate and unload plugin schema functionality
    These feature additions allow for plugin:
    
    * validating of schema; validates and tests loading of plugin schema
    * unloading of plugin schemas; useful for removing a plugin schema
      during runtime

--- plugin/validator.go
+++ plugin/validator.go
@@ -22,9 +22,18 @@ func NewValidator(opts ValidatorOpts) (*Validator, error) {
 	return &Validator{vm: vm}, nil
 }
 
+func (v *Validator) ValidateSchema(schema string) (string, error) {
+	return v.vm.CallByParams("validate_plugin_schema", schema)
+}
+
 func (v *Validator) LoadSchema(schema string) (string, error) {
 	pluginName, err := v.vm.CallByParams("load_plugin_schema", schema)
 	return pluginName, err
+}
+
+func (v *Validator) UnloadSchema(schema string) error {
+	_, err := v.vm.CallByParams("unload_plugin_schema", schema)
+	return err
 }
 
 func (v *Validator) Validate(pluginInstance string) error {
