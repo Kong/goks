3aaa8e04385878820c7171bb44383ff8d2d7af1c
Gabriele Gerbino <gabrielegerbino@gmail.com>
2022-07-18 16:26:56 +0200

    fix: use ngx.null for nil check on null map values
    Right now, whenever a map containing some nil values is passed
    to the process_auto_fields function, the code fails to
    recognize it applying the wrong logic to it.
    
    This commit makes sure this doesn't happen by performing the
    nil check on `ngx.null` instead of `nil`.

--- lua-tree/share/lua/5.1/kong/db/schema/init.lua
+++ lua-tree/share/lua/5.1/kong/db/schema/init.lua
@@ -1667,7 +1667,7 @@ function Schema:process_auto_fields(data, context, nulls, opts)
         end
 
       elseif ftype == "string" then
-        if is_insert_or_upsert and value == nil then
+        if is_insert_or_upsert and value == null then
           value = random_string()
         end
 
